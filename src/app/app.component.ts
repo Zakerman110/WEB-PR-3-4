edf08f3a7c8ad047368d0f4c3e846d2814a51e4c
Artur Hostiuk <artur1100011@gmail.com>
2021-11-04 13:40:30 +0200

    complete Lab8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  tab : any = "tab2";
+  tab1 : any
+  tab2 : any
+
+  onClick(check){
+        if(check==1){
+          this.tab = 'tab1';
+        }else if(check==2){
+          this.tab = 'tab2';
+        }   
+      
+    }
+
+    onSwitch(check){ 
+      switch (check) {
+       case 1:
+         return 'tab1';
+       case 2:
+         return 'tab2';
+     }
+    }
 }
